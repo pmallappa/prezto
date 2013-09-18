61d6b5b7b3bcf4ed4a9fa9b7ae6aad28a49e463b
Prem Mallappa <pmallappa@caviumnetworks.com>
2013-09-19 01:13:31 +0530

    Fixed loading linux modules on osx

--- prem_modules/linux/init.zsh
+++ prem_modules/linux/init.zsh
@@ -1,5 +1,9 @@
 ### linux / zeesh! plugin
 
+if [ "$(uname)" != "Linux" ]; then
+	return
+fi
+
 setopt extended_glob
 #export LS_COLORS='di=1;34:ln=35:so=32:pi=33;40:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 
