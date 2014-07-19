d059f46f6a582bd05b05be047170d00f1189ace2
Prem Mallappa <pmallapp@broadcom.com>
2014-07-19 18:01:38 +0530

    Added z module, now looks for go installation

---
+++ prem_modules/z/init.zsh
@@ -0,0 +1,21 @@
+typeset -g goroot_paths
+goroot_paths=($HOME/'devel/compilers/goroot')
+
+function set_go_paths() {
+    if (( $+commands[go] )); then
+        go env | while read line; do
+                     [[ "$line" =~ "TERM*" ]] && continue
+                     eval export $line
+		 done
+	export PATH=${PATH}:${GOBIN}
+    else
+
+	for p in $goroot_paths; do
+	    echo $p
+	    [ -e "$p/bin/go" ] && export GOROOT=$p && break
+	done
+	
+	if [ -z "$GOROOT" ] && echo "GOROOT not set, probably not installed"
+    fi
+}
+#export set_go_paths
